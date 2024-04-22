c1c5c370f4fa71dfd8ba593f1ef09c79d68a7b82
Emanuel <emanuelnas29@gmail.com>
2024-04-21 22:50:19 -0300

    heranças e POO

---
+++ 10_contrutores_destrutores/construtor.py
@@ -0,0 +1,23 @@
+# Exemplo contrutor 
+
+class Cachorro: 
+    def __init__(self, nome, cor, acordado=True):
+        print("inicializando a classe...")
+        self.nome = nome
+        self.cor = cor
+        self.acordado = acordado
+
+    def __del__(self):
+        print("Removendo a instancia da classe.")
+    
+    def falar(self):
+        print("auauau")
+
+def criar_cachorro():
+    c = Cachorro("zeus", " branco e preto", False)
+    print(c.nome)
+
+#c = Cachorro("Nala", "Branca")
+#c.falar()
+
+criar_cachorro()
---
+++ 10_contrutores_destrutores/destrutor.py
@@ -0,0 +1,8 @@
+# Exemplo Destrutor 
+
+class Cachorro:
+    def __del__(self):
+        print("Destruindo a instancia")
+
+c = Cachorro()
+del c
